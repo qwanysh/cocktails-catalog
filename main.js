5488a7a4591b5bbb98081788ddbc78f6d1cc8251
qwanysh <olimpsdu@gmail.com>
2020-08-25 23:02:53 +0600

    Add cocktail details modal with ingredients

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ $('#searchButton').on('click', function (event) {
 });
 
 const renderCocktails = cocktails => {
+  const cocktailContainer = $('#cocktailsContainer');
+  cocktailContainer.html('');
+
   for (const cocktail of cocktails) {
     renderCocktail(cocktail);
   }
@@ -32,5 +35,68 @@ const renderCocktail = cocktail => {
     </div>
   `);
 
+  const cocktailDetailsButton = cocktailCard.find('a.btn');
+  cocktailDetailsButton.on('click', () => showDetailModal(cocktail));
+
   cocktailContainer.append(cocktailCard);
+};
+
+const showDetailModal = cocktail => {
+  const detailModal = $('<div class="modal">');
+
+  const ingredients = getIngredientsOfCocktail(cocktail);
+
+  const ingredientsHtml = ingredients.map((ingredient) => {
+    return `
+      <li class="media">
+        <img src="${ingredient.imageUrl}" class="mr-3" alt="${ingredient.name}" style="width: 10%">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">${ingredient.name}${ingredient.measure ? ` (${ingredient.measure})` : ''}</h5>
+        </div>
+      </li>
+    `;
+  });
+
+  detailModal.html(`
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">${cocktail.strDrink}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <h5>Ingredients:</h5>
+          <ul class="list-unstyled">
+            ${ingredientsHtml.join('')}
+          </ul>
+          <h5>Instructions:</h5>
+          <p>${cocktail.strInstructions}</p>
+        </div>
+      </div>
+    </div>
+  `);
+
+  $('body').append(detailModal);
+  detailModal.modal();
+};
+
+const getIngredientsOfCocktail = cocktail => {
+  const ingredients = [];
+  let index = 1;
+  let ingredientName = cocktail[`strIngredient${index}`];
+
+  while (ingredientName) {
+    const ingredientObject = {
+      name: ingredientName,
+      measure: cocktail[`strMeasure${index}`],
+      imageUrl: `https://www.thecocktaildb.com/images/ingredients/${ingredientName}.png`,
+    };
+    ingredients.push(ingredientObject);
+    index++;
+    ingredientName = cocktail[`strIngredient${index}`];
+  }
+
+  return ingredients;
 };
